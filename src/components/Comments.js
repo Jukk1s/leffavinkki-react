5bef8798737dcf8a9f190de48e4541c814f5e172
eljash <eljas.hirvela@metropolia.fi>
2022-03-10 17:44:25 +0200

    Kommentit tulevat uudesta vanhempaan järjestyksessä.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,13 +4,14 @@ import SingleComment from "./SingleComment";
 const server = "http://localhost:8081";
 
 export default function Comments ({data}) {
+    //data.map(comment=>{
     if(data!==undefined)
         return(
             <div className="comments">
                 {
                     data.map(comment=>{
                         return <SingleComment data={comment}/>
-                    })
+                    }).reverse()
                 }
             </div>
         )
